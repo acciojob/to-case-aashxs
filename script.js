6739000358fd309db7014214507792508477b9cc
Ashish <90548985+aashxs@users.noreply.github.com>
2024-08-12 14:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function toCase(text) {
   // write your code here
 	if(text.length==0){
-		return "-";
+		let a="-";
+		return a;
 	}
 	else{
-		let new=text.toUpperCase;
+		let new1=text.toUpperCase;
 		let new2=text.toLowerCase;
-		lt ans=`${new2}-${new}`;
+		let ans=`${new2}-${new1}`;
 		return ans;
 	}
 }
